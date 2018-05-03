1533f6a8997fc7aa9d901e4952484604fcdd2efa
LeoLaugier <leojlaugier@gmail.com>
2018-05-02 23:17:21 -0700

    tested version of scatter_broadcast

--- integrated_helper.py
+++ integrated_helper.py
@@ -16,14 +16,15 @@ def scatter_broadcast(data, datadims, batch_split, nodes_model, nodes_batch, com
 
 	if rank == 0:
 		buf = []
-		tmpX = x.flatten().tolist()
+		tmpX = data.flatten().tolist()
 
 		for i in range(size):
-			batch_split_i = i % nodes_batch
+			batch_split_i = i // nodes_model
 			start = elementsPerBatchSplit * batch_split_i * sizeofdp
 			end = start + elementsPerBatchSplit * sizeofdp
 			buf.append(tmpX[start:end])
 
+		
 		ind = 0
 		curr = 0
 		for elem in tmpX[elementsPerBatchSplit * sizeofdp * nodes_batch:]:
@@ -31,7 +32,7 @@ def scatter_broadcast(data, datadims, batch_split, nodes_model, nodes_batch, com
 				ind += 1
 				curr = 0
 			for j in range(nodes_model):
-				buf[ind*j].append(elem)
+				buf[ind*nodes_model+j].append(elem)
 			curr += 1
 
 	recvbuf = comm.scatter(buf, root=0)
@@ -39,6 +40,7 @@ def scatter_broadcast(data, datadims, batch_split, nodes_model, nodes_batch, com
 	size_batch_split = datadims[0] // nodes_batch
 	if batch_split < datadims[0] % nodes_batch:
 		size_batch_split += 1 
+
 
 	if not recvbuf:
 		return np.array([])
@@ -73,7 +75,7 @@ if __name__ == "__main__":
 
 	nodes_model = 3
 	nodes_batch = 2
-	batch_split = rank // nodes_batch
+	batch_split = rank // nodes_model
 
 
 
@@ -84,9 +86,11 @@ if __name__ == "__main__":
 		x = np.random.rand(5,2)
 		y = np.random.rand(5,1)
         print(x)
+        comm.Barrier()
 
 	x = scatter_broadcast(x, (5,2), batch_split, nodes_model, nodes_batch, comm, rank, size)
 	y = scatter_broadcast(y, (5,1), batch_split, nodes_model, nodes_batch, comm, rank, size)  
+	
 	if rank == 0:
 		print("rank: ", rank, "my data: ", x, y)
 		comm.Barrier()
@@ -100,5 +104,12 @@ if __name__ == "__main__":
 		comm.Barrier()
 
 	elif rank == 3:
+		print("rank: ", rank, "my data: ", x, y)
+		comm.Barrier()
+
+	elif rank == 4:
+		print("rank: ", rank, "my data: ", x, y)
+		comm.Barrier()
+	elif rank == 5:
 		print("rank: ", rank, "my data: ", x, y)
 		comm.Barrier()
